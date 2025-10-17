472ffaf1a7f024c38be1174e17c58f6753418efa
forrest1398 <129135885+forrest1398@users.noreply.github.com>
2025-10-17 23:19:19 +0900

    feat : 해상도 조절 기능 추가 및 html 수정

--- src/main/java/com/example/AsciiConverter.java
+++ src/main/java/com/example/AsciiConverter.java
@@ -9,32 +9,57 @@ import java.io.IOException;
 
 public class AsciiConverter {
     public static void convert() throws IOException {
-        // get gray image
+
+        // 흑백 이미지 불러오기
         BufferedImage grayImage = ImageIO.read(new File("./res/img/output_gray.png"));
         int width = grayImage.getWidth();
         int height = grayImage.getHeight();
 
-        // get gray level and create textfile
+        // 명암에 따른 ASCII 문자
         String asciiList = "@&#%Y!*+=- ";
 
-        File outputAsciiFile = new File("./res/html/output_ascii.html");
+        // 해상도 조절
+        int scope= 2;
+        File outputAsciiFile = new File("./res/html/output_ascii_scope_"+scope+".html");
+
+        // html 생성
         BufferedWriter writer = new BufferedWriter(new FileWriter(outputAsciiFile));
-        writer.write("<!DOCTYPE html>");
-        writer.newLine();
-        writer.write("<html>");
-        writer.write("<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />");
-        writer.newLine();
-        writer.write("<head>");
-        writer.newLine();
-        writer.write("<title>ACSII ART</title>");
-        writer.newLine();
-        writer.write("</head>");
-        writer.newLine();
-        writer.write("<body style=\"margin:0; font-family: \'consolas\'; line-height: 85%; letter-spacing:2px; font-size: 10px; align-content: center \"  >");
-        writer.newLine();
-        writer.write("<pre>");
-        for (int row = 0; row < height; row++) {
-            for (int col = 0; col < width; col++) {
+        writer.write("<!DOCTYPE html>\n<html>\n<head>\n<meta charset='UTF-8'>\n<title>ASCII Art</title>\n");
+        writer.write("<style>\n");
+
+        // body style
+        writer.write("body {\n");
+        writer.write("  margin: 0;\n");
+        writer.write("  align-items: center;\n");
+        writer.write("}\n");
+
+        // pre style
+        writer.write("pre {\n");
+        writer.write("  margin: 0;\n");
+        writer.write("  white-space: pre;\n");
+        // 가로와 세로 중, 긴 쪽을 기준
+        if (width > height) {
+            // 가로가 더 긴 경우 → 가로를 100vw 기준으로 맞춤
+            writer.write("  font-size: calc(100vw / " + (width / scope) + ");\n");
+        } else {
+            // 세로가 더 긴 경우 → 세로를 100vh 기준으로 맞춤
+            writer.write("  font-size: calc(100vh / " + (height / scope) + ");\n");
+        }
+
+        writer.write("  letter-spacing: 2px;\n");
+        writer.write("  transform-origin: top left;\n");
+        writer.write("}\n");
+        writer.write("</style>\n");
+        writer.write("</head>\n<body>\n");
+
+        // 원본 이미지
+        writer.write("<img src='../img/input.png' alt='Original Image'>\n");
+        // 흑백 이미지
+        writer.write("<img src='../img/output_gray.png' alt='Gray Image'>\n");
+
+        writer.write("<pre>\n");
+        for (int row = 0; row < height; row+=scope) {
+            for (int col = 0; col < width; col+=scope) {
                 int grayLevel= (grayImage.getRGB(col, row)) & 0xFF;
                 int asciiLevel = (int)Math.ceil(grayLevel/25.6);
                 char a = asciiList.charAt(asciiLevel);
@@ -42,11 +67,7 @@ public class AsciiConverter {
             }
             writer.newLine();
         }
-        writer.write("</pre>");
-        writer.newLine();
-        writer.write("</body>");
-        writer.newLine();
-        writer.write("</html>");
+        writer.write("</pre>\n</body>\n</html>");
         writer.close();
     }
 }
